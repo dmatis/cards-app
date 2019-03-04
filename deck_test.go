c1259bfa364098505e8d4889a81ddd8e091a2f7d
dmatis <darren.matis@gmail.com>
2019-03-04 09:48:39 -0800

    Added additional tests

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,27 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != expectedLength {
 		t.Errorf("Expected deck length of %v but got %v", expectedLength, len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected King of Clubs but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
